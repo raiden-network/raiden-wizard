d49cf4cda1a126d5b56617cbbd34672c01a29342
Nils Diefenbach <nlsdfnbch.foss@kolabnow.com>
2019-03-13 11:39:48 +0100

    Sketch out EthClientInstallationStep class.

--- raiden-installer/steps/eth_client.py
+++ raiden-installer/steps/eth_client.py
@@ -1,76 +1,101 @@
 import pathlib
 
-from installer.utils import create_symlink, create_desktop_icon, download_file
-from installer.constants import PATHS
+from raiden_installer.constants import PATHS, CLIENTS
+from raiden_installer.steps.executor import StepExecutor
+from raiden_installer.utils import (
+    create_symlink,
+    create_desktop_icon,
+    download_file,
+    extract_archive,
+    user_input
+)
 
 
-def download_eth_binary(target_path: pathlib.Path) -> pathlib.Path:
-    """Download the latest binary of 'the' ethereum client.
+class EthClientInstallationStep(StepExecutor):
 
-    Flip a coin on whether to use geth or parity? User selection would be
-    fine too, I guess.
+    def __init__(self, install_dir: pathlib.Path = PATHS.DEFAULT_INSTALL_DIR):
+        super(EthClientInstallationStep, self).__init__('eth_client', install_dir)
+        self.download_dir = self.install_dir.joinpath('download')
+        self.archive = None
+        self.binary_dir = self.install_dir.joinpath('bin')
+        self.binary = None
 
-    TODO: This is a stub.
-    """
-    return download_file(target_path, "version_string")
+    def download_binary(self):
+        """Download the latest Ethereum client binary.
 
+        TODO: This is a stub.
+        """
+        self.archive = download_file(self.download_dir, self.client_version)
 
-def install_eth_binary(archive: pathlib.Path, target_path: pathlib.Path) -> pathlib.Path:
-    """ Extract a downloaded binary from its archive and install it in the given directory.
+    def install_binary(self):
+        """Install the binary on this machine, unpacking the archive if necessary.
 
-    TODO: This is a stub.
-    """
-    return archive, target_path
+        TODO: This is a stub.
+        """
+        self.binary = extract_archive(self.archive, self.binary_dir)
 
+    def configure_client(self, method: str) -> None:
+        """configure the client to use the given `method`.
 
-def configure_eth_client(bin_path: pathlib.Path, method: str):
-    """Configure the Ethereum client using the given method.
+        TODO: This is a stub.
+        """
 
-    TODO: This is a stub.
-    """
-    return bin_path, method
+    def install_private_chain() -> None:
+        """Install a private chain and connect to it.
 
+        TODO: This is a stub (and will probably remain one for quite a while).
+        """
 
-def install_private_chain() -> None:
-    """Install a private chain and connect to it.
+    def run(self) -> None :
+        """Execute the Ethereum Client installation step.
 
-    TODO: This is a stub (and will probably remain one for quite a while).
-    """
+        TODO: This is a stub.
+        """
+        eth_client = user_input(
+            "Use local eth client? [Yes/no]",
+            default='yes',
+            options=['yes', 'no']
+        )
+        if eth_client == 'no':
+            self.download_binary()
+            self.install_binary()
+            make_symlink = user_input(
+                "Create a symbolic link at /usr/local/bin for the Ethereum client? [Yes/no]",
+                default='yes',
+                options=['yes', 'no'],
+                short_hand=True
+            )
+            if make_symlink == 'yes':
+                create_symlink(self.binary)
 
+            desktop_icon = user_input(
+                'Would you like to create a desktop icon for the Ethereum client?',
+                default='yes',
+                options=['yes', 'no'],
+                short_hand=True
+            )
+            if desktop_icon == 'yes':
+                create_desktop_icon(self.binary)
+        else:
+            self.binary = user_input(
+                'Please specify the path to the eth client: [/usr/local/bin/geth]',
+                default=PATHS.USR_BIN_DIR.join(CLIENTS.GETH),
+                short_hand=True
+            )
+        # Determine which connection method we should use.
+        connection_method = user_input(
+            "Your selection: [1]",
+            default=1,
+            options=[
+                'Connect to Infura',
+                'Connect to an existing Ethereum Client',
+                'Connect to an existing Raiden Client (launches WebUI after installation)',
+                'Use local Ethereum Client and synchronize network',
+                'Install a private chain'
+            ]
+        )
 
-def install_eth_client(download_cache_dir: pathlib.Path, bin_path: pathlib.Path) -> None :
-    """Execute the Ethereum Client installation step.
-
-    TODO: This is a stub.
-    """
-    # TODO: User input require input validation.
-    eth_client = input("Use local eth client? [Y/n]")
-    if not eth_client:
-        eth_archive_path = download_eth_binary(download_cache_dir)
-        eth_client_path = install_eth_binary(eth_archive_path, bin_path)
-        make_symlink = input("Create a symbolic link at /usr/local/bin for the Ethereum client? [Y/n]")
-        if make_symlink:
-            create_symlink(eth_client_path)
-
-        desktop_icon = user_input('Would you like to create a desktop icon for the Ethereum client?')
-        if desktop_icon:
-            create_desktop_icon(eth_client_path)
-    else:
-        # TODO: User input require input validation.
-        eth_client_path = user_input('Please specify the path to the eth client: [/usr/local/bin/geth]', default=PATHS.DEFAULT_INSTALL_DIR.join(CLIENTS.GETH))
-
-    # Determine which connection method we should use.
-    print(
-        "\nPlease choose a connection option:"
-        "   [1] Connect to Infura"
-        "   [2] Connect to an existing Ethereum Client"
-        "   [3] Connecto to an existing Raiden Client (launches WebUI after installation)"
-        "   [4] Use local Ethereum Client and synchronize network"
-        "   [5] Install a private chain\n"
-    )
-    connection_method = input("Your selection: [1]")
-
-    if connection_method == 5:
-        install_private_chain()
-    else:
-        configure_eth_client(eth_client_path, connection_method)
+        if connection_method == 5:
+            self.install_private_chain()
+        else:
+            self.configure_client(connection_method)

7ba987739139bf714dd91773e1acf4cac06079b7
Nils Diefenbach <nlsdfnbch.foss@kolabnow.com>
2019-03-13 11:57:41 +0100

    Integrate token acquisition funcs into TokenAcquisitionStep.

--- raiden-installer/steps/tokens.py
+++ raiden-installer/steps/tokens.py
@@ -1,22 +1,25 @@
 from raiden_installer.steps.executor import StepExecutor
-from raiden_installer.utils import user_input
+from raiden_installer.utils import user_input, is_testnet
 
 
 class TokenAcquisitionStep(StepExecutor):
 
-    def acquire_token(self, network: str, token: str) -> None:
+    def __init__(self, network, account):
+        self.account = account
+        self.network = network
+
+    def acquire_token(self, token: str) -> None:
         """Acquire the given `token` on the given `network`.
 
         TODO: This is a stub.
         """
 
-    def token_acquisition(self, network, is_testnet):
+    def token_acquisition(self):
         """Execute the token acquisition step.
 
         TODO: This is a stub.
         """
-        if is_testnet(network):
-            # TODO: User input require input validation.
+        if is_testnet(self.network):
             token = user_input('Specify a token to acquire:')
             self.acquire_token(token)
         else:

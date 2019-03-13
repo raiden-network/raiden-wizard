5bccab46217c01ff1af3bb50a41fb11d0c10bbc1
Nils Diefenbach <nlsdfnbch.foss@kolabnow.com>
2019-03-13 11:53:36 +0100

    Integrate funding funcs in FundingStep class.

--- raiden-installer/steps/funding.py
+++ raiden-installer/steps/funding.py
@@ -3,29 +3,36 @@ from raiden_installer.utils import is_testnet
 
 
 class AccountFundingStep(StepExecutor):
+    def __init__(self, account, network, is_new_account=False):
+        super(AccountFundingStep, self).__init__('funding')
+        self.account = account
+        self.network = network
+        self.is_new_account = is_new_account
 
-    def request_testnet_ether(self, account: str, network: str):
-        """Request ether for the given `account`
+    def request_testnet_ether(self):
+        """Request ether for the given `account` from somewhere.
 
         TODO: This is a stub.
         """
-        return account, network
 
-    def fund_mainnet_account(self, account):
+    def fund_mainnet_account(self):
         """Magically fund your main net account out of thin air.
 
         ..or something.
 
         TODO: This is a stub.
         """
-        return account
 
-    def fund_account(self, network, account):
-        if is_testnet(network):
-            self.request_testnet_ether(account, network)
+    def run(self):
+        """Execute the account funding step.
+
+        TODO This is a stub.
+        """
+        if is_testnet(self.network):
+            self.request_testnet_ether()
         else:
-            if create_account:
-                self.fund_mainnet_account(account)
+            if self.is_new_account:
+                self.fund_mainnet_account()
             else:
                 # Do nothing - existing accounts should be funded by the user.
                 pass

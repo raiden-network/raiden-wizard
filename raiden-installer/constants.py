234df5cf4cd89b3d87aacbb21595d3bcc12ca4ce
Nils Diefenbach <nlsdfnbch.foss@kolabnow.com>
2019-03-13 13:50:44 +0100

    Add default keystore paths for eth clients.

--- raiden-installer/constants.py
+++ raiden-installer/constants.py
@@ -2,12 +2,7 @@ import pathlib
 import sys
 
 
-if sys.platform == 'darwin':
-    ARCHIVE_EXT = 'zip'
-    PLATFORM = 'macOS'
-else:
-    ARCHIVE_EXT = 'tar.gz'
-    PLATFORM = 'linux'
+ARCHIVE_EXT = 'zip' if sys.platform == 'darwin' else 'tar.gz'
 
 
 class PATHS:
@@ -26,16 +21,18 @@ class RAIDEN_META:
 class GETH_META:
     NAME = 'geth'
     VERSION = '1.8.23'
-    FLAVOR = sys.platform
+    ARCH = sys.platform
     COMMIT = 'c9427004'
-    DOWNLOAD_URL = f'https://gethstore.blob.core.windows.net/builds/geth-{FLAVOR}-{VERSION}-{COMMIT}.tar.gz'
+    DOWNLOAD_URL = f'https://gethstore.blob.core.windows.net/builds/geth-{ARCH}-{VERSION}-{COMMIT}.tar.gz'
     SIGNATURE = f'{DOWNLOAD_URL}.asc'
     BIN_PATH = PATHS.USR_BIN_DIR.joinpath(NAME)
+    KEYSTORE_PATH = pathlib.Path.home().joinpath('.ethereum')
 
 
 class PARITY_META:
     NAME = 'parity'
     VERSION = 'v2.4.0'
-    FLAVOR = 'apple-darwin' if sys.platform == 'darwin' else 'unknown-linux-gnu'
-    DOWNLOAD_URL = f'https://releases.parity.io/ethereum/{VERSION}/x86_64-{FLAVOR}/parity'
+    ARCH = 'apple-darwin' if sys.platform == 'darwin' else 'unknown-linux-gnu'
+    DOWNLOAD_URL = f'https://releases.parity.io/ethereum/{VERSION}/x86_64-{ARCH}/parity'
     BIN_PATH = PATHS.USR_BIN_DIR.joinpath(NAME)
+    KEYSTORE_PATH = pathlib.Path.home().joinpath('.local', 'share', 'io.parity.ethereum', 'keys')

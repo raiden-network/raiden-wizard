788195f7d369ae37f2535d5a4023ed019df76e57
taleldayekh <mail@talel.se>
2019-06-14 11:40:23 +0200

    Function for downloading Raiden archive

--- raiden_installer/raiden.py
+++ raiden_installer/raiden.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import json
 
@@ -10,7 +11,7 @@ def latest_raiden_release() -> str:
     except requests.exceptions.RequestException as err:
         print(
             'Could not retrieve latest release data from the GitHub API,'
-            + ' please try again later'
+            ' please try again later'
         )
 
     try:
@@ -19,12 +20,12 @@ def latest_raiden_release() -> str:
     except json.JSONDecodeError as err:
         print(
             'Could not retrieve latest release data, response object is'
-            + ' not valid JSON'
+            ' not valid JSON'
         )
     except IndexError as err:
         print(
             'Could not retrieve latest release data, index of list does'
-            + ' not exist'
+            ' not exist'
         )
     except KeyError as err:
         print('Could not retrieve "tag_name" from JSON response object')
@@ -42,11 +43,29 @@ def raiden_download_url(raiden_release: str, platform: str) -> str:
 
     raiden_download_url = (
         'https://github.com/raiden-network/raiden/releases/download/'
-        + f'{raiden_release}/raiden-'
-        + f'{raiden_release}-{platform}-x86_64.{archive}'
+        f'{raiden_release}/raiden-'
+        f'{raiden_release}-{platform}-x86_64.{archive}'
     )
     return raiden_download_url
 
 
 def download_raiden_archive(raiden_download_url: str, dest_dir: str) -> str:
-    pass
+    try:
+        res = requests.get(raiden_download_url)
+        file_content = res.content
+    except requests.exceptions.RequestException as err:
+        print(
+            'Could not retrieve Raiden archive from GitHub,'
+            ' please try again later'
+        )
+
+    filename = os.path.basename(raiden_download_url)
+    archive = os.path.join(dest_dir, filename)
+
+    try:
+        with open(archive, 'wb') as f:
+            f.write(file_content)
+
+        return archive
+    except OSError as err:
+        print('Unable to download archive')

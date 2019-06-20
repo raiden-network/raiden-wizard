5f542a1592311c9d5178cc6a194adca7ad116e48
taleldayekh <mail@talel.se>
2019-06-20 13:55:42 +0200

    Check for available port and launch browser on run

--- raiden_installer/installer_server.py
+++ raiden_installer/installer_server.py
@@ -1,3 +1,5 @@
+import webbrowser
+from socket import socket
 from flask import Flask
 
 
@@ -14,4 +16,15 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    new_socket = socket(socket.AF_INET, socket.SOCK_STREAM)
+    new_socket.bind(('127.0.0.1', 0))
+    port = new_socket.getsockname()[1]
+
+    # Jumps over port where Raiden will be running
+    if port == 5001:
+        port += 1
+
+    new_socket.close()
+
+    webbrowser.open_new(f'http://127.0.0.1:{port}/')
+    app.run(host='127.0.0.1', port=f'{port}')

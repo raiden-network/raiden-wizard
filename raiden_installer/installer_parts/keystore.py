c6ef3b9115e74d892adf573e3e4fa9dbc5a68669
taleldayekh <mail@talel.se>
2019-06-23 21:12:47 +0200

    make_keystore function

--- raiden_installer/installer_parts/keystore.py
+++ raiden_installer/installer_parts/keystore.py
@@ -1,5 +1,9 @@
+import os
+import json
+from pathlib import Path
 from datetime import datetime
 from uuid import uuid4
+from eth_keyfile import create_keyfile_json
 
 
 def generate_keyfile_name() -> str:
@@ -8,3 +12,20 @@ def generate_keyfile_name() -> str:
 
     keyfile_name = f'UTC--{now_formatted}Z--{uuid4()!s}'
     return keyfile_name
+
+
+def make_keystore(dest_dir: str, keyfile_name: str, keystore_pwd: str) -> dict:
+    '''
+    Creates a keystore directory with a
+    passphrase encrypted keystore file.
+    '''
+    keystore = Path(dest_dir).joinpath('keystore')
+    keystore.mkdir(exist_ok=True)
+
+    keyfile = Path(keystore).joinpath(keyfile_name)
+    keyfile_content = create_keyfile_json(os.urandom(32), keystore_pwd.encode())
+
+    with open(keyfile, 'w') as f:
+        json.dump(keyfile_content, f)
+
+    return keyfile_content

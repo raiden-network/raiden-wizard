a1f5f47a6db47471e29291d7f4482759529e1803
taleldayekh <mail@talel.se>
2019-06-26 17:10:02 +0200

    Installation step 1-4

--- raiden_installer/installer_server.py
+++ raiden_installer/installer_server.py
@@ -1,3 +1,5 @@
+import json
+import time
 import socket
 import webbrowser
 from flask import (
@@ -7,14 +9,24 @@ from flask import (
     redirect,
     url_for
 )
+from web3 import Web3, HTTPProvider
+from eth_utils import to_checksum_address
+from installer_parts import (
+    keystore,
+    raiden_config,
+    funding
+)
+from constants import (
+    DEST_DIR,
+    TOKEN_AMOUNT,
+    GAS_PRICE,
+    GAS_MINT,
+    GAS_APPROVE,
+    GAS_DEPOSIT
+)
 
 
 app = Flask(__name__)
-
-
-global keystore_pwd
-global network
-global proj_id
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -24,13 +36,86 @@ def user_input():
         network = request.form['network']
         proj_id = request.form['proj-id']
 
-        return redirect(url_for('installation'))
+        '''
+        Installation Step 1
+
+        Create keystore directory and
+        retrieve keyfile and address.
+        '''
+        keyfile = keystore.make_keystore(
+            DEST_DIR,
+            keystore.generate_keyfile_name(),
+            keystore_pwd
+        )
+
+        with open(keyfile, 'r') as f:
+            keyfile_content = json.load(f)
+
+        address = keyfile_content['address']
+
+        '''
+        Installation Step 2
+
+        Build the ETH RPC endpoint URL
+        '''
+        eth_rpc = raiden_config.eth_rpc_endpoint(proj_id, network)
+
+        '''
+        Installation Step 3
+
+        Acquire ETH and use Web3 to confirm that
+        the account address has a balance.
+        '''
+        eth_funding = funding.goerli_funding(address)
+        w3 = Web3(HTTPProvider(eth_rpc))
+
+        while w3.eth.getBalance(to_checksum_address(address)) <= 0:
+            time.sleep(1)
+            print('Not enough ETH on account address')
+
+        '''
+        Installation Step 4
+
+        Acquire tokens for PFS and Monitoring services
+        and retrieve the user deposit contract address.
+        '''
+        private_key = keystore.get_private_key(keyfile_content, keystore_pwd)
+
+        pfs_monitoring_funding = funding.PfsAndMonitoringFunding(
+            w3,
+            to_checksum_address(address),
+            private_key
+        )
+        user_deposit_address = pfs_monitoring_funding.user_deposit_address
+
+        mint_tokens = pfs_monitoring_funding.mint_tokens(
+            TOKEN_AMOUNT,
+            GAS_MINT,
+            GAS_PRICE
+        )
+        print(mint_tokens)
+
+        approve_deposit = pfs_monitoring_funding.approve_deposit(
+            TOKEN_AMOUNT,
+            GAS_APPROVE,
+            GAS_PRICE
+        )
+        print(approve_deposit)
+
+        make_deposit = pfs_monitoring_funding.make_deposit(
+            TOKEN_AMOUNT,
+            GAS_DEPOSIT,
+            GAS_PRICE
+        )
+        print(make_deposit)
+
+        '''
+        Installation Step 5
+
+        Generate TOML config file
+        '''
+
     return render_template('user-input.html')
-
-
-@app.route('/installation', methods=['GET'])
-def installation():
-    return render_template('installation.html')
 
 
 if __name__ == '__main__':

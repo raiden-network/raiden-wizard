72cf15a5e9199c60cb5f0cbb6e4a0ab1513db2f7
taleldayekh <mail@talel.se>
2019-06-28 12:04:36 +0200

    Added constants

--- raiden_installer/constants.py
+++ raiden_installer/constants.py
@@ -1,4 +1,16 @@
 import sys
+from raiden_contracts.constants import GAS_REQUIRED_FOR_UDC_DEPOSIT
 
 
 PLATFORM = 'macOS' if sys.platform == 'darwin'  else 'linux'
+
+DEST_DIR = '/Users/taleldayekh/desktop/installer'
+
+
+# Constants for PFS and Monitoring funding
+GAS_PRICE = 2_000_000_000
+GAS_MINT = 70_000
+GAS_APPROVE = 70_000
+GAS_DEPOSIT = GAS_REQUIRED_FOR_UDC_DEPOSIT * 2
+
+TOKEN_AMOUNT = 10 ** 18

a1e05c64ad8567fa6048a15c96dea2c0dad5aff8
taleldayekh <mail@talel.se>
2019-06-28 16:01:23 +0200

    Hooks for PyInstaller

---
+++ raiden_installer/pyinstaller_hooks/hook-raiden_contracts.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+from raiden_contracts.contract_manager import _BASE
+
+
+datas = []
+
+for subdir, _, _ in os.walk(_BASE):
+    for file_path in Path(subdir).glob('*.json'):
+        datas.append((str(file_path), os.path.basename(subdir)))

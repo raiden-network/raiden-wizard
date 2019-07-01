7e74b170f121c6348644bbb9137f382a668db5cf
taleldayekh <mail@talel.se>
2019-07-01 14:20:28 +0200

    Passed failing tests

--- tests/test_raiden_config.py
+++ tests/test_raiden_config.py
@@ -11,11 +11,11 @@ def test_create_plain_txt_pwd_file_path(mock_open_file):
     from the class variable once a plain txt pwd file
     has been created.
     '''
-    plain_txt_pwd = raiden_config.PlainTxtPwd('/dest/dir/path', 'password')
+    plain_txt_pwd = raiden_config.PlainTxtPwd('/config/dir/path', 'password')
     plain_txt_pwd.create_plain_txt_pwd_file()
 
     pwd_file = plain_txt_pwd.pwd_file
-    assert str(pwd_file) == '/dest/dir/path/pwd.txt'
+    assert str(pwd_file) == '/config/dir/path/pwd.txt'
 
 
 def test_eth_rpc_endpoint_url():
@@ -41,9 +41,9 @@ def test_generate_raiden_config_file_data(mock_toml, mock_open_file):
     '''
     config_data = {
         "environment-type": "development",
-        "keystore-path": Path("/dest/dir/path/keystore"),
+        "keystore-path": "/keystore/dir/path/keystore",
         "address": "0x4a01C085EA70Ae37487641cb78fa973BB6C310a4",
-        "password-file": Path("/dest/dir/path/pwd.txt"),
+        "password-file": "/config/dir/path/pwd.txt",
         "user-deposit-contract-address": (
             "0x6978D210a7F69527a210C0942ED7520045FE1a29"
         ),
@@ -54,13 +54,14 @@ def test_generate_raiden_config_file_data(mock_toml, mock_open_file):
         ),
         "routing-mode": "pfs",
         "pathfinding-service-address": (
-            "https://pfs-goerli.services-dev.raiden-network"
+            "https://pfs-goerli.services-dev.raiden.network"
         ),
         "enable-monitoring": True
     }
 
     raiden_config.generate_raiden_config_file(
-        '/dest/dir/path',
+        '/config/dir/path',
+        '/keystore/dir/path',
         'https://goerli.infura.io/v3/6a9a5919fc3e4d2088b2512b0da8926a',
         '4a01c085ea70ae37487641cb78fa973bb6c310a4',
         '0x6978D210a7F69527a210C0942ED7520045FE1a29'
@@ -76,9 +77,10 @@ def test_generate_raiden_config_file_returns_path(mock_open_file):
     Tests that path to the generated config file is returned
     '''
     config_file = raiden_config.generate_raiden_config_file(
-        '/dest/dir/path',
+        '/config/dir/path',
+        '/keystore/dir/path',
         'https://goerli.infura.io/v3/6a9a5919fc3e4d2088b2512b0da8926a',
         '4a01c085ea70ae37487641cb78fa973bb6c310a4',
         '0x6978D210a7F69527a210C0942ED7520045FE1a29'
     )
-    assert config_file == Path('/dest/dir/path/config.toml')
+    assert config_file == Path('/config/dir/path/config.toml')

721b968db7046d81ed4277eced2efe5d05f1fc21
taleldayekh <mail@talel.se>
2019-07-13 11:17:29 +0200

    New launcher page with list of all configurations

--- installer/web/app.py
+++ installer/web/app.py
@@ -1,53 +1,40 @@
-import socket
 import webbrowser
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
-from ..base import (
-    Account,
-    Network,
-    RaidenClient,
-    RaidenConfigurationFile,
-    build_infura_url,
-    is_valid_infura_project_id,
-)
+from ..base import RaidenConfigurationFile
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
-def install_raiden():
-    if request.method == "POST":
-        passphrase = request.form["keystore-pwd"]
-        project_id = request.form["proj-id"]
+def launch_screen():
+    configuration_files = RaidenConfigurationFile.get_available_configurations()
 
-        if not passphrase == "" and is_valid_infura_project_id(project_id):
-            account = Account.create(passphrase)
-            network = Network.get_by_name("goerli")
-            ethereum_client_rpc_endpoint = build_infura_url(network, project_id)
+    # TODO: This commented out code should be refactored in accordance with the new lauhc screen
+    # if request.method == "POST":
+    # passphrase = request.form["keystore-pwd"]
+    # project_id = request.form["proj-id"]
 
-            raiden_configuration_file = RaidenConfigurationFile(
-                account, network, ethereum_client_rpc_endpoint
-            )
-            raiden_configuration_file.save()
+    # if not passphrase == "" and is_valid_infura_project_id(project_id):
+    #     account = Account.create(passphrase)
+    #     network = Network.get_by_name("goerli")
+    #     ethereum_client_rpc_endpoint = build_infura_url(network, project_id)
 
-            RaidenClient.get_latest_release().install()
-        else:
-            return """ERROR"""
+    #     raiden_configuration_file = RaidenConfigurationFile(
+    #         account, network, ethereum_client_rpc_endpoint
+    #     )
+    #     raiden_configuration_file.save()
 
-    return render_template("install-raiden.html")
+    #     RaidenClient.get_latest_release().install()
+    # else:
+    #     return """ERROR"""
+
+    return render_template(
+        "launch-screen.html", configuration_files=configuration_files
+    )
 
 
 if __name__ == "__main__":
-    new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    new_socket.bind(("127.0.0.1", 0))
-    port = new_socket.getsockname()[1]
-
-    # Skips port where Raiden will be running
-    if port == 5001:
-        port += 1
-
-    new_socket.close()
-
-    webbrowser.open_new(f"http://127.0.0.1:{port}/")
-    app.run(host="127.0.0.1", port=f"{port}")
+    webbrowser.open_new("http://127.0.0.1:5000/")
+    app.run(host="127.0.0.1", port="5000")

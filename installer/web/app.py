40f5a624fe8de6639064292b1bdf6bcae897fb72
taleldayekh <mail@talel.se>
2019-07-14 13:12:27 +0200

    Uncommented code

--- installer/web/app.py
+++ installer/web/app.py
@@ -11,24 +11,23 @@ app = Flask(__name__)
 def launch_screen():
     configuration_files = RaidenConfigurationFile.get_available_configurations()
 
-    # TODO: This commented out code should be refactored in accordance with the new lauhc screen
-    # if request.method == "POST":
-    # passphrase = request.form["keystore-pwd"]
-    # project_id = request.form["proj-id"]
+    if request.method == "POST":
+    passphrase = request.form["keystore-pwd"]
+    project_id = request.form["proj-id"]
 
-    # if not passphrase == "" and is_valid_infura_project_id(project_id):
-    #     account = Account.create(passphrase)
-    #     network = Network.get_by_name("goerli")
-    #     ethereum_client_rpc_endpoint = build_infura_url(network, project_id)
+    if not passphrase == "" and is_valid_infura_project_id(project_id):
+        account = Account.create(passphrase)
+        network = Network.get_by_name("goerli")
+        ethereum_client_rpc_endpoint = build_infura_url(network, project_id)
 
-    #     raiden_configuration_file = RaidenConfigurationFile(
-    #         account, network, ethereum_client_rpc_endpoint
-    #     )
-    #     raiden_configuration_file.save()
+        raiden_configuration_file = RaidenConfigurationFile(
+            account, network, ethereum_client_rpc_endpoint
+        )
+        raiden_configuration_file.save()
 
-    #     RaidenClient.get_latest_release().install()
-    # else:
-    #     return """ERROR"""
+        RaidenClient.get_latest_release().install()
+    else:
+        return """ERROR"""
 
     return render_template(
         "launch-screen.html", configuration_files=configuration_files

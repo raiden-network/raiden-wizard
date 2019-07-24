20416b231a150e30733a96587f7fad02cd7e7fc6
Raphael Lullis <raphael@brainbot.com>
2019-07-24 03:35:49 +0200

    Quick shell script to run all commands necessary to build the static binary

---
+++ tools/build_static_binary.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Assuming the virtualenv is already activated
+
+PWD=$(dirname "$0")
+PROJECT_ROOT="$PWD/.."
+DIST_FOLDER="$PROJECT_ROOT/dist"
+BINARY_NAME="raiden_wizard"
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    printf "Virtualenv environment needs to be set"
+    exit;
+fi
+
+pip install -r "$PROJECT_ROOT/requirements.txt" -I
+
+pip install pyinstaller staticx
+pyinstaller "$PWD/pyinstaller/raiden_webapp.spec"
+staticx "$DIST_FOLDER/$BINARY_NAME" "${PROJECT_ROOT}/${BINARY_NAME}.${OSTYPE}.bin"

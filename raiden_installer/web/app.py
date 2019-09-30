727d1b5acec11d238b7849aa6454093e2a7ece35
Raphael Lullis <raphael@brainbot.com>
2019-09-30 13:43:16 +0200

    Launch action done directly by sending message to websocket + unified method to toggle view from main panel and screen with message updates

--- raiden_installer/web/app.py
+++ raiden_installer/web/app.py
@@ -17,7 +17,7 @@ from ..network import Network
 from ..raiden import RaidenClient, RaidenClientError
 from ..ethereum_rpc import Infura, EthereumRPCProvider, make_web3_provider
 from ..tokens import EthereumAmount, RDNAmount, DAIAmount, Wei
-from ..token_exchange import Exchange, Kyber, Uniswap
+from ..token_exchange import Exchange
 
 
 DEBUG = "RAIDEN_INSTALLER_DEBUG" in os.environ
@@ -69,63 +69,76 @@ class TokenExchangeForm(Form):
 
 
 class AsyncTaskHandler(WebSocketHandler):
-    def _send_status_update(self, message_text, message_type="info", complete=False):
-        self.write_message(
-            json.dumps({"type": "status-update", "text": message_text, "complete": complete})
-        )
+    def _send_status_update(self, message_text):
+        self.write_message(json.dumps({"type": "status-update", "text": message_text}))
         log.info(message_text)
 
+    def _send_error_message(self, error_message):
+        self.write_message(json.dumps({"type": "error-message", "text": error_message}))
+        log.error(error_message)
 
-class ExchangeRateTrackerHandler(AsyncTaskHandler):
-    def _serialize_quote(self, exchange, token_amount, account):
-        costs = exchange.calculate_transaction_costs(token_amount, account)
-        total_price = costs and costs.get("total")
+    def _send_task_complete(self, message_text):
+        self.write_message(json.dumps({"type": "task-complete", "text": message_text}))
+        log.info(message_text)
 
-        return total_price and {"wei": total_price.as_wei, "formatted": total_price.formatted}
+    def _send_redirect(self, redirect_url):
+        self.write_message(json.dumps({"type": "redirect", "redirect_url": redirect_url}))
+        log.info(f"Redirecting to {redirect_url}")
 
-    def open(self, configuration_file_name):
+    def on_message(self, message):
+        data = json.loads(message)
+
+        method = data.pop("method", None)
+        action = method and {
+            "close": self._run_close,
+            "launch": self._run_launch,
+            "swap": self._run_swap,
+        }.get(method)
+
+        return action and action(**data)
+
+    def _run_close(self, **kw):
+        sys.exit()
+
+    def _run_launch(self, **kw):
+        configuration_file_name = kw.get("configuration_file_name")
         configuration_file = RaidenConfigurationFile.get_by_filename(configuration_file_name)
-        w3 = make_web3_provider(
-            configuration_file.ethereum_client_rpc_endpoint, configuration_file.account
+
+        network = configuration_file.network
+        client_class = RaidenClient.select_client_class(network)
+        latest = client_class.get_latest_release()
+        if not latest.is_installed:
+            self._send_status_update(f"Downloading and installing raiden {latest.release}")
+            latest.install()
+            self._send_status_update("Installation complete")
+
+        self._send_status_update(
+            "Launching Raiden, this might take a couple of minutes, do not close the browser"
         )
 
-        kyber = Kyber(w3=w3)
-        uniswap = Uniswap(w3=w3)
+        if not latest.is_running:
+            latest.launch(configuration_file)
 
-        is_kyber_listing = kyber.is_listing_token("RDN")
-        is_uniswap_listing = uniswap.is_listing_token("RDN")
-
-        for amount in FUNDING_AMOUNTS:
-            rdn_amount = RDNAmount(amount)
-            data = {"type": "new-quote", "token_amount": str(rdn_amount.as_wei)}
-
-            if is_kyber_listing:
-                data["kyber_price"] = self._serialize_quote(
-                    kyber, rdn_amount, configuration_file.account
-                )
-
-            if is_uniswap_listing:
-                data["uniswap_price"] = self._serialize_quote(
-                    uniswap, rdn_amount, configuration_file.account
-                )
-
-            self.write_message(json.dumps(data))
-
-
-class SwapStatusNotificationHandler(AsyncTaskHandler):
-    def on_message(self, message):
-        DEPOSIT_TIMEOUT = 5
         try:
-            data = json.loads(message)
-            configuration_file = RaidenConfigurationFile.get_by_filename(
-                data["configuration_file_name"]
-            )
+            latest.wait_for_web_ui_ready()
+            self._send_task_complete("Raiden is ready!")
+            self._send_redirect(latest.WEB_UI_INDEX_URL)
+        except (RaidenClientError, RuntimeError) as exc:
+            self._send_error_message(f"Raiden process failed to start: {exc}")
+            latest.kill()
+
+    def _run_swap(self, **kw):
+        DEPOSIT_TIMEOUT = 5
+        configuration_file_name = kw.get("configuration_file_name")
+
+        try:
+            configuration_file = RaidenConfigurationFile.get_by_filename(configuration_file_name)
             form = TokenExchangeForm(
                 {
                     "network": [configuration_file.network.name],
-                    "exchange": [data["exchange"]],
-                    "token_amount": [data["amount"]],
-                    "token_sticker": [data["token"]],
+                    "exchange": [kw["exchange"]],
+                    "token_amount": [kw["amount"]],
+                    "token_sticker": [kw["token"]],
                 }
             )
 
@@ -159,116 +172,9 @@ class SwapStatusNotificationHandler(AsyncTaskHandler):
             else:
                 for key, error_list in form.errors.items():
                     error_message = f"{key}: {'/'.join(error_list)}"
-                    self._send_status_update(message_type="error", message_text=error_message)
+                    self._send_error_message(error_message)
         except (json.decoder.JSONDecodeError, KeyError) as exc:
-            self._send_status_update(message_type="error", message_text=str(exc))
-
-
-class LauncherStatusNotificationHandler(AsyncTaskHandler):
-    def open(self, configuration_file_name):
-        configuration_file = RaidenConfigurationFile.get_by_filename(configuration_file_name)
-
-        account = configuration_file.account
-        network = configuration_file.network
-        # w3 = make_web3_provider(configuration_file.ethereum_client_rpc_endpoint, account)
-
-        # raiden_token = RaidenTokenNetwork(w3=w3, account=account)
-        # custom_token = CustomTokenNetwork(w3=w3, account=account)
-
-        # ethereum_amount = network.MINIMUM_ETHEREUM_BALANCE_REQUIRED
-
-        # has_faucet = network.FAUCET_AVAILABLE
-        # needed_funds = ethereum_amount - account.get_ethereum_balance(w3)
-        # needs_funding = any(
-        #     [
-        #         raiden_token.is_available(network) and not raiden_token.is_funded,
-        #         custom_token.is_available(network) and not custom_token.is_funded,
-        #     ]
-        # )
-
-        # try:
-        #     if needs_funding and needed_funds <= 0:
-        #         self._send_status_update(f"Account is funded with enough ETH to swap for tokens")
-        #     elif needs_funding and needed_funds > 0 and has_faucet:
-        #         self._send_status_update(f"Funding account with {network.capitalized_name} ETH")
-        #         network.fund(account)
-        #         self._send_status_update(f"ETH successfully acquired", message_type="success")
-        #     elif needs_funding:
-        #         self._send_status_update(
-        #             f"Please send at least {needed_funds:0.4f} ETH to 0x{account.address} on "
-        #             f"{network.capitalized_name}"
-        #         )
-        #         account.wait_for_ethereum_funds(w3, network)
-        #     else:
-        #         self._send_status_update(f"ETH funding not required at the moment")
-        #     self._send_status_update(
-        #         f"Current balance: {account.get_ethereum_balance(w3):0.4f} ETH"
-        #     )
-        # except Exception as exc:
-        #     self._send_status_update(
-        #         f"Failed to add funds to account: {exc}", message_type="warning"
-        #     )
-
-        # if custom_token.is_available(network):
-        #     if not custom_token.is_funded:
-        #         try:
-        #             self._send_status_update(f"Obtaining tokens")
-        #             custom_token.mint(CustomTokenNetwork.TOKEN_AMOUNT)
-        #             custom_token.deposit(CustomTokenNetwork.TOKEN_AMOUNT)
-        #             self._send_status_update(
-        #                 "Raiden successfully funded with tokens", message_type="success"
-        #             )
-        #         except Exception as exc:
-        #             self._send_status_update(
-        #                 f"Failed to pre-mint custom tokens.", message_type="error"
-        #             )
-        #             self._send_status_update(
-        #                 f"You should still be able to do it on Raiden Web UI: {exc}",
-        #                 message_type="error",
-        #             )
-        #     else:
-        #         self._send_status_update(
-        #             f"Account already funded with {custom_token.balance:0.4f} WIZ"
-        #         )
-
-        # if raiden_token.is_available(network):
-        #     if not raiden_token.is_funded:
-        #         try:
-        #             self._send_status_update(f"Running ETH <-> RDN Swap")
-        #             exchange = Exchange.select_by_rate(w3, account, ethereum_amount)
-        #             exchange_rate = exchange.get_current_rate(ethereum_amount)
-        #             self._send_status_update(
-        #                 f"{exchange.name} has the best rate of {exchange_rate:0.4f} RDN/ETH"
-        #             )
-        #             exchange.swap_ethereum_for_rdn(ethereum_amount, exchange_rate)
-        #         except ExchangeError as exc:
-        #             self._send_status_update(
-        #                 f"Failed to exchange ETH for RDN: {exc}", message_type="error"
-        #             )
-        #     else:
-        #         self._send_status_update(f"Account has already {raiden_token.balance:0.4f} RDN")
-
-        client_class = RaidenClient.select_client_class(network)
-        latest = client_class.get_latest_release()
-        if not latest.is_installed:
-            self._send_status_update(f"Downloading and installing raiden {latest.release}")
-            latest.install()
-            self._send_status_update("Installation complete")
-
-        self._send_status_update(
-            "Launching Raiden, this might take a couple of minutes, do not close the browser"
-        )
-
-        if not latest.is_running:
-            latest.launch(configuration_file)
-
-        try:
-            latest.wait_for_web_ui_ready()
-            self._send_status_update("Raiden is ready!")
-        except RaidenClientError as exc:
-            self._send_status_update(f"Raiden process failed to start: {exc}")
-        else:
-            sys.exit()
+            self._send_error_message(str(exc))
 
 
 class IndexHandler(RequestHandler):
@@ -278,35 +184,8 @@ class IndexHandler(RequestHandler):
 
 class SetupHandler(RequestHandler):
     def get(self):
-        self.render(
-            "raiden_setup.html",
-            configuration_file_names=[
-                os.path.basename(f) for f in RaidenConfigurationFile.list_existing_files()
-            ],
-        )
-
-
-class LaunchHandler(RequestHandler):
-    def get(self, configuration_file_name):
-        tracker_websocket_url = "ws://{host}{path}".format(
-            host=self.request.host,
-            path=self.reverse_url("launch_tracker", configuration_file_name),
-        )
-
-        try:
-            configuration_file = RaidenConfigurationFile.get_by_filename(configuration_file_name)
-        except ValueError as exc:
-            self.set_status(400)
-            self.write(str(exc))
-            self.finish()
-
-        raiden_client = RaidenClient.select_client_class(configuration_file.network)
-
-        self.render(
-            "launch.html",
-            tracker_websocket_url=tracker_websocket_url,
-            redirect_url=raiden_client.WEB_UI_INDEX_URL,
-        )
+        file_names = [os.path.basename(f) for f in RaidenConfigurationFile.list_existing_files()]
+        self.render("raiden_setup.html", configuration_file_names=file_names)
 
 
 class AccountDetailHandler(RequestHandler):
@@ -383,7 +262,6 @@ class ConfigurationItemAPIHandler(APIHandler):
             {
                 "file_name": configuration_file.file_name,
                 "account_page_url": self.reverse_url("account", configuration_file.file_name),
-                "launch_page_url": self.reverse_url("launch", configuration_file.file_name),
                 "funding_page_url": self.reverse_url("funding", configuration_file.file_name),
                 "account": configuration_file.account.address,
                 "network": configuration_file.network.name,
@@ -399,7 +277,7 @@ if __name__ == "__main__":
             url(r"/setup", SetupHandler, name="setup"),
             url(r"/account/(.*)", AccountDetailHandler, name="account"),
             url(r"/funding/(.*)", AccountFundingHandler, name="funding"),
-            url(r"/launch/(.*)", LaunchHandler, name="launch"),
+            url(r"/ws", AsyncTaskHandler, name="websocket"),
             url(
                 r"/api/configurations", ConfigurationListAPIHandler, name="api-configuration-list"
             ),

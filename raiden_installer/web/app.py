d1fc4c87eab3189a9c86a663af1d986f5183bcb6
Raphael Lullis <raphael@brainbot.com>
2019-10-22 18:58:15 +0200

    Simplified styling of action div (TIL about justify-content: space-evenly). Added fetch of ETH/RDN exchange rate from kyber and use it to find out if there are enough funds to allow user to swap

--- raiden_installer/web/app.py
+++ raiden_installer/web/app.py
@@ -23,8 +23,8 @@ DEBUG = "RAIDEN_INSTALLER_DEBUG" in os.environ
 PORT = 8080
 
 
-MINIMUM_RDN_REQUIRED_AS_WEI = 6 * (10 ** 18)
-MINIMUM_ETH_REQUIRED_AS_WEI = 2 * (10 ** 16)
+MINIMUM_RDN_REQUIRED = RDNAmount(Wei(6 * (10 ** 18)))
+MINIMUM_ETH_REQUIRED = EthereumAmount(Wei(5 * (10 ** 15)))
 
 AVAILABLE_NETWORKS = [Network.get_by_name(n) for n in ["mainnet", "ropsten", "goerli"]]
 NETWORKS_WITH_TOKEN_SWAP = [Network.get_by_name(n) for n in ["mainnet", "ropsten"]]
@@ -208,8 +208,8 @@ class BaseRequestHandler(RequestHandler):
         context_data.update(
             {
                 "network": DEFAULT_NETWORK,
-                "minimum_eth_required_as_wei": MINIMUM_ETH_REQUIRED_AS_WEI,
-                "minimum_rdn_required_as_wei": MINIMUM_RDN_REQUIRED_AS_WEI,
+                "minimum_eth_required": MINIMUM_ETH_REQUIRED,
+                "minimum_rdn_required": MINIMUM_RDN_REQUIRED,
             }
         )
         return super().render(template_name, **context_data)
@@ -405,7 +405,7 @@ class ConfigurationItemAPIHandler(APIHandler):
 if __name__ == "__main__":
     app = Application(
         [
-            url(r"/", IndexHandler),
+            url(r"/", IndexHandler, name="index"),
             url(r"/configurations", ConfigurationListHandler, name="configuration-list"),
             url(r"/setup", SetupHandler, name="setup"),
             url(r"/account/(.*)", AccountDetailHandler, name="account"),

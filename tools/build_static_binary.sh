bb433bd873cbee766895b2e4e47e54a8d73eb3a4
Raphael Lullis <raphael@brainbot.com>
2019-11-25 17:37:35 +0100

    Add settings.toml to gitignore. Updated build script.

--- tools/build_static_binary.sh
+++ tools/build_static_binary.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-# Assuming the virtualenv is already activated
+if [ "$#" -ne 1 ]; then
+    echo "Usage: build_static_binary.sh path/to/configuration.toml"
+    exit -1
+fi
 
 PWD=$(dirname "$0")
+
+# realpath is not always available on macOS, so we resort to get the path via python
+SETTINGS_SOURCE_FILE=`python -c "import os; print(os.path.abspath('$1'))"`
+
 PROJECT_ROOT="$PWD/.."
 DIST_FOLDER="$PROJECT_ROOT/dist"
 BINARY_NAME="raiden_wizard"
+SETTINGS_FILE="$PROJECT_ROOT/resources/conf/settings.toml"
 
+# Virtualenv must be activated
 if [ -z "$VIRTUAL_ENV" ]; then
-    printf "Virtualenv environment needs to be set"
-    exit;
+    echo "Error: python virtualenv needs to be activated"
+    exit -2
 fi
+
+rm $SETTINGS_FILE
+cp $SETTINGS_SOURCE_FILE $SETTINGS_FILE
 
 pip install -r "$PROJECT_ROOT/requirements.txt" -I
 

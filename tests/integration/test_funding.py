4882630f84caaccc55645b23ab15c126273e0f3d
manuelwedler <manuel.wedler@googlemail.com>
2020-09-08 15:57:12 +0200

    Fix mint token tests

--- tests/integration/test_funding.py
+++ tests/integration/test_funding.py
@@ -10,7 +10,8 @@ from raiden_installer.account import Account
 from raiden_installer.ethereum_rpc import Infura, make_web3_provider
 from raiden_installer.network import Network
 from raiden_installer.tokens import Erc20Token, EthereumAmount, Wei
-from raiden_installer.transactions import mint_tokens
+from raiden_installer.transactions import get_token_balance, mint_tokens
+from raiden_installer.utils import wait_for_transaction
 
 INFURA_PROJECT_ID = os.getenv("TEST_RAIDEN_INSTALLER_INFURA_PROJECT_ID")
 
@@ -53,10 +54,14 @@ class TokenTestCase(IntegrationTestCase):
         super().setUp()
         self.svt_token = Erc20Token.find_by_ticker("SVT", self.NETWORK_NAME)
 
-    def test_can_not_mint_tokens_without_gas(self):
+    def test_cannot_mint_tokens_without_gas(self):
         with self.assertRaises(ValueError):
             mint_tokens(w3=self.w3, account=self.account, token=self.svt_token)
 
     def test_can_mint_tokens(self):
         self.network.fund(self.account)
-        mint_tokens(w3=self.w3, account=self.account, token=self.svt_token)
+        self.account.wait_for_ethereum_funds(self.w3, EthereumAmount(0.01))
+        tx_hash = mint_tokens(w3=self.w3, account=self.account, token=self.svt_token)
+        wait_for_transaction(self.w3, tx_hash)
+        balance = get_token_balance(self.w3, self.account, self.svt_token)
+        self.assertTrue(balance.as_wei >= self.svt_token.supply)
